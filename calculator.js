6bfe8ab90eee77f3f0f9e0567505567bde46b66e
Justin <jreaylayton@gmail.com>
2021-06-23 22:24:16 -0700

    Simplified and added all planned functionality.

--- calculator.js
+++ calculator.js
@@ -1,105 +1,159 @@
-const display = document.querySelector(".display");
-const operands = document.querySelectorAll(".operand");
-const operators = document.querySelectorAll(".operator");
-const editors = document.querySelectorAll(".editor");
+const operandButtons = document.querySelectorAll('[data-operand]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const clearButton = document.querySelector('[data-clear]');
+const negateButton = document.querySelector('[data-negate]');
+const displayField = document.querySelector('[data-display]');
 
-operands.forEach(operand => operand.addEventListener("click", handleOperand));
-operators.forEach(operand => operand.addEventListener("click", handleOperator));
-editors.forEach(editor => editor.addEventListener("click", handleEditor));
+let displayVal = '';
+let prevOperand = '';
+let currOperand = '';
+let currOperation = '';
 
-const calculator = {
-    displayValue: "0",
-    operand1: null,
-    operand2: null,
-    operating: false,
-    operator: null,
-}
+window.addEventListener('keypress', (e) => {
+    //handle
+});
+
+operandButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        appendNum(button.innerText);
+        updateDisplay();
+    });
+});
+
+operationButtons.forEach(button => {
+    button.addEventListener('click', () => {
+        chooseOperation(button.innerText);
+    });
+});
+
+equalsButton.addEventListener('click', () => {
+    operate(currOperation, prevOperand, currOperand);
+    updateDisplay();
+});
+
+clearButton.addEventListener('click', () => {
+    clear();
+    updateDisplay();
+});
+
+deleteButton.addEventListener('click', () => {
+    deleteNum();
+    updateDisplay();
+});
+
+negateButton.addEventListener('click', () => {
+    negate();
+    updateDisplay();
+});
 
 function add(a, b) {
     return a + b;
-};
+}
 
 function subtract(a, b) {
     return a - b;
-};
+}
 
 function multiply(a, b) {
     return a * b;
-};
+}
 
 function divide(a, b) {
     return a / b;
-};
+}
 
-function inverse(a) {
-    return 1 / a;
-};
-
-function square(a) {
-    return a * a;
-};
-
-function sqrt(a) {
-    return Math.sqrt(a);
-};
+function power(a, b) {
+    return a ** b;
+}
 
 function clear() {
-    calculator.displayValue = "0";
-    calculator.operand1 = null;
-    calculator.operand2 = null;
-    calculator.operating = false;
-    calculator.operator = null;
-    updateDisplay();
-};
+    displayVal = '';
+    currOperand = '';
+    prevOperand = '';
+    currOperation = '';
+}
 
-function clearEntry() {
+function deleteNum() {
+    if (displayVal === '') {
+        clear(); 
+        updateDisplay();
+        return;
+    }
+    displayVal = displayVal.slice(0, -1);
+}
 
+function appendNum(num) {
+    displayVal += num.toString();
+    currOperand += num.toString();
+}
+
+function chooseOperation(operation) {
+    if (currOperand === '' && prevOperand === '' && currOperation === '') return;
+    if (currOperation !== '' && currOperation !== operation) {
+        currOperation = operation;
+        return;
+    }
+
+    if (currOperand === '' && currOperation !== '') {
+        operate(currOperation, prevOperand, prevOperand);
+        updateDisplay();
+        currOperation = operation;
+    }
+    else if (currOperand !== '' && prevOperand !== '' && currOperand !== '') {
+        operate(currOperation, prevOperand, currOperand);
+        updateDisplay();
+        currOperation = operation;
+        currOperand = '';
+        displayVal = '';
+    }
+    else {
+        currOperation = operation;
+        prevOperand = currOperand;
+        currOperand = '';
+        displayVal = '';
+    }
+}
+
+function operate(operator, a, b) {
+    if (displayVal === 'ERROR') return;
+    a = parseFloat(a);
+    b = parseFloat(b);
+    if (isNaN(a) || isNaN(b)) return;
+    let result;
+    switch (operator) {
+        case '^':
+            result = power(a, b);
+            break;
+        case '+':
+            result = add(a, b);
+            break;
+        case '-':
+            result = subtract(a, b);
+            break;
+        case '*':
+            result = multiply(a, b);
+            break;
+        case '/':
+            if (b === 0) result = 'ERROR';
+            else result = divide(a, b);
+            break;
+        default:
+            return;
+    }
+    prevOperand = result;
+    displayVal = result;
+    currOperand = '';
+    currOperation = '';
+}
+
+function negate() {
+    if (displayVal === '') return;
+    displayVal = -displayVal;
+    currOperand = -currOperand;
 }
 
 function updateDisplay() {
-    display.textContent = calculator.displayValue;
+    displayField.innerText = displayVal;
 }
-
-
-function operate(operator, a, b) {
-    a = parseInt(a);
-    b = parseInt(b);
-    let result;
-    switch (operator) {
-        case "+":
-            result = add(a, b);
-            break;
-        case "-":
-            result = subtract(a, b);
-            break;
-        case "x":
-            result = multiply(a, b);
-            break;
-        case "/":
-            result = divide(a, b);
-            break;
-        default:
-            break;                   
-    }
-    return result;
-};
-
-function erase() {
-    calculator.displayValue = calculator.displayValue.slice(0, -1);
-    display.textContent = (calculator.displayValue === "") ? "0" : calculator.displayValue;
-};
-
-function handleEditor() {
-    switch (this.id) {
-        case "C":
-            clear();
-            break;
-        case "CE":
-            clearEntry();
-            break;
-        case "E":
-            erase();
-            break;         
-    }
-};
-
