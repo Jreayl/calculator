268074fc031c830ae0287d3293e3a01b720ba832
Jreayl <65147261+Jreayl@users.noreply.github.com>
2021-07-10 20:37:16 -0700

    Add files via upload
    Revised code and fixed bugs.

--- README.md
+++ README.md
@@ -1,20 +1,5 @@
 # The Odin Project - Web Development #
 ## Project: Calculator ##
 The final project in the 'Foundations' section of TOP is to make a web 
-based calculator using HTML, CSS, and JavaScript. The current version is mostly
-complete, but it is still a work in progress since there are still some bugs to iron out
-and improvements to the Javascript code to be made.
-
-### Features: ###
-* power
-* add
-* subtract
-* multiply
-* divide
-* negate
-* all clear
-* delete (backspace)
-* decimals
-
-### Features to be implemented: ###
-* keyboard support
+based calculator using HTML, CSS, and JavaScript. This calculator has
+keyboard support.
--- calculator.js
+++ calculator.js
@@ -1,52 +1,30 @@
-const operandButtons = document.querySelectorAll('[data-operand]');
-const operationButtons = document.querySelectorAll('[data-operation]');
-const equalsButton = document.querySelector('[data-equals]');
-const deleteButton = document.querySelector('[data-delete]');
-const clearButton = document.querySelector('[data-clear]');
-const negateButton = document.querySelector('[data-negate]');
-const displayField = document.querySelector('[data-display]');
+const numberBtns = document.querySelectorAll('[data-number]');
+const operatorBtns = document.querySelectorAll('[data-operator]');
+const equalsBtn = document.querySelector('[data-equals]');
+const deleteBtn = document.querySelector('[data-delete]');
+const clearBtn = document.querySelector('[data-clear]');
+const negateBtn = document.querySelector('[data-negate]');
+const display = document.querySelector('[data-display]');
 
-let displayVal = '';
+numberBtns.forEach(button => {
+    button.addEventListener('click', () => appendNum(button.innerText));
+});
+
+operatorBtns.forEach(button => {
+    button.addEventListener('click', () => setOperation(button.innerText));
+});
+
+equalsBtn.addEventListener('click', calculate);
+deleteBtn.addEventListener('click', deleteNum);
+clearBtn.addEventListener('click', clear);
+negateBtn.addEventListener('click', negate);
+window.addEventListener('keydown', keyInput);
+
+const keyOperators = ['+', '-', '*', '/', '^'];
+
 let prevOperand = '';
 let currOperand = '';
-let currOperation = '';
-
-window.addEventListener('keypress', (e) => {
-    //handle
-});
-
-operandButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        appendNum(button.innerText);
-        updateDisplay();
-    });
-});
-
-operationButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        chooseOperation(button.innerText);
-    });
-});
-
-equalsButton.addEventListener('click', () => {
-    operate(currOperation, prevOperand, currOperand);
-    updateDisplay();
-});
-
-clearButton.addEventListener('click', () => {
-    clear();
-    updateDisplay();
-});
-
-deleteButton.addEventListener('click', () => {
-    deleteNum();
-    updateDisplay();
-});
-
-negateButton.addEventListener('click', () => {
-    negate();
-    updateDisplay();
-});
+let currOperation = undefined;
 
 function add(a, b) {
     return a + b;
@@ -68,59 +46,9 @@ function power(a, b) {
     return a ** b;
 }
 
-function clear() {
-    displayVal = '';
-    currOperand = '';
-    prevOperand = '';
-    currOperation = '';
-}
-
-function deleteNum() {
-    if (displayVal === '') {
-        clear(); 
-        updateDisplay();
-        return;
-    }
-    displayVal = displayVal.slice(0, -1);
-}
-
-function appendNum(num) {
-    displayVal += num.toString();
-    currOperand += num.toString();
-}
-
-function chooseOperation(operation) {
-    if (currOperand === '' && prevOperand === '' && currOperation === '') return;
-    if (currOperation !== '' && currOperation !== operation) {
-        currOperation = operation;
-        return;
-    }
-
-    if (currOperand === '' && currOperation !== '') {
-        operate(currOperation, prevOperand, prevOperand);
-        updateDisplay();
-        currOperation = operation;
-    }
-    else if (currOperand !== '' && prevOperand !== '' && currOperand !== '') {
-        operate(currOperation, prevOperand, currOperand);
-        updateDisplay();
-        currOperation = operation;
-        currOperand = '';
-        displayVal = '';
-    }
-    else {
-        currOperation = operation;
-        prevOperand = currOperand;
-        currOperand = '';
-        displayVal = '';
-    }
-}
-
 function operate(operator, a, b) {
-    if (displayVal === 'ERROR') return;
-    a = parseFloat(a);
-    b = parseFloat(b);
-    if (isNaN(a) || isNaN(b)) return;
+    a = Number(a);
+    b = Number(b);
     let result;
     switch (operator) {
         case '^':
@@ -132,28 +60,93 @@ function operate(operator, a, b) {
         case '-':
             result = subtract(a, b);
             break;
-        case '*':
+        case '×':
             result = multiply(a, b);
             break;
-        case '/':
-            if (b === 0) result = 'ERROR';
-            else result = divide(a, b);
+        case '÷':
+            if (b === 0) {
+                alert("I'm sorry, Dave. I'm afraid I can't do that.");
+                clear();
+                return '';
+            }
+            result = divide(a, b);
             break;
         default:
-            return;
+            result = '';
+            break;   
     }
-    prevOperand = result;
-    displayVal = result;
+    result = (result.toString().length >= 12) ? formatLargeNum(result) : result;
+    return result;
+}
+
+function calculate() {
+    if (currOperation === undefined) return;
+    if (currOperand === '' && prevOperand === '') return;
+    if (prevOperand !== '' && currOperand !== '') {
+        currOperand = operate(currOperation, prevOperand, currOperand); // 1 + 1 + => 2 or 1 + 1 = => 2
+    } else {
+        currOperand = operate(currOperation, prevOperand, prevOperand); // 1 + + => 2
+    }
+    currOperation = undefined;
+    updateDisplay();
+}
+
+function appendNum(number) {
+    if (number === '.' && currOperand.includes('.')) return;
+    if (number === '.' && currOperand.length >= 11 || currOperand.length >= 12) return;
+    currOperand += number;
+    updateDisplay();
+}
+
+function deleteNum() {
+    currOperand = currOperand.slice(0, -1);
+    updateDisplay();
+}
+
+function clear() {
+    prevOperand = '';
     currOperand = '';
-    currOperation = '';
+    currOperation = undefined;
+    updateDisplay();
 }
 
 function negate() {
-    if (displayVal === '') return;
-    displayVal = -displayVal;
+    if (currOperand === '') return;
     currOperand = -currOperand;
+    updateDisplay();
 }
 
 function updateDisplay() {
-    displayField.innerText = displayVal;
+    display.innerText = currOperand;
+}
+
+function convertOperation(operation) {
+    if (operation === '+') return '+';
+    if (operation === '-') return '-';
+    if (operation === '*') return '×';
+    if (operation === '/') return '÷';
+    if (operation === '^') return '^';
+}
+
+function setOperation(operation) {
+    if (currOperation !== undefined) calculate();
+    currOperation = operation;
+    prevOperand = currOperand;
+    currOperand = '';
+}
+
+function formatLargeNum(number) {
+    result = Number(number);
+    if (result.toString().includes('.')) result = Math.round(Number(result));
+    else result = result.toExponential(2);
+    return result;
+}
+
+function keyInput(e) {
+    if (e.key >= 0 && e.key <= 9 || e.key === '.') appendNum(e.key);
+    if (keyOperators.indexOf(e.key) !== -1) setOperation(convertOperation(e.key));
+    if (e.key === 'Enter') calculate();
+    if (e.key === 'Backspace') deleteNum();
+    if (e.key === 'Escape') clear();
+    if (e.key === '!') negate();
 }
